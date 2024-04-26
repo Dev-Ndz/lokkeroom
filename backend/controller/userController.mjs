dab49b7ccc75c452e6de0722ae1fef1d60bb8dca
ndz <nicolas.dzierzgowski@gmail.com>
2024-04-26 16:53:46 +0200

    tests

--- backend/controller/userController.mjs
+++ backend/controller/userController.mjs
@@ -1,16 +1,18 @@
 import User from "../Model/user.mjs"
 
-
 export const getLobbies = async(req, res) => {
+    console.log("coucou")
+    return res.send("coucou")
     try{
+        console.log("WE ARE IN !!!")
         const lobbyList = await User.lobbies(req.user.id);
-        return lobbyList;
+        return res.send("coucou");
     }catch(err){
         return res.status(500).send(err);
     }
 }
 
-export const getUsername = (req, res) => {
+// export const getUsername = (req, res) => {
 
-}
+// }
 
--- backend/routes/user.mjs
+++ backend/routes/user.mjs
@@ -3,7 +3,8 @@ import {getLobbies, getUsername}from '../controller/userController.mjs'
 
 const router = express.Router();
 
-router.get('/lobbies',getLobbies);
+// router.get('/lobbies',getLobbies);
+router.get('/lobbies',async (req, res) => { res.send("hello from user/lobbies")});
 router.get('/username',getUsername);
 
 
