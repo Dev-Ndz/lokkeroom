3c8de660dd03b7c16a3881a37aa6231fc44c5af7
ndz <nicolas.dzierzgowski@gmail.com>
2024-04-26 16:59:17 +0200

    fixing the fixes

--- backend/controller/userController.mjs
+++ backend/controller/userController.mjs
@@ -2,11 +2,11 @@ import User from "../Model/user.mjs"
 
 export const getLobbies = async(req, res) => {
     console.log("coucou")
-    return res.send("coucou")
+
     try{
         console.log("WE ARE IN !!!")
         const lobbyList = await User.lobbies(req.user.id);
-        return res.send("coucou");
+        return res.send(lobbyList);
     }catch(err){
         return res.status(500).send(err);
     }
