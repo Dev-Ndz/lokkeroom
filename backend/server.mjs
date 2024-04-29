43f88ecb26f70cffa0c3292ef8d876b5da8f1ded
ndz <nicolas.dzierzgowski@gmail.com>
2024-04-29 14:58:41 +0200

    fixing cors

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,14 +14,12 @@ app.use(cors());
 app.use(express.json())
 
 const server = http.createServer(app)
-const io = new Server(server,
-  {
+const io = new Server(server,{
   cors:{
-    origin:"http://localhost:5173/",
+    origin:"http://localhost:5173",
     methods:["GET","POST"]
   }
-}
-)
+})
 
 
 
