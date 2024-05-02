db762e43b5352c3eb0679f1be6b614a689b0feaa
ndz <nicolas.dzierzgowski@gmail.com>
2024-05-02 19:53:55 +0200

    commented out socket.io

--- backend/server.mjs
+++ backend/server.mjs
@@ -7,19 +7,19 @@ import { messagesRoute } from './routes/messages.mjs'
 import { userRoute } from './routes/user.mjs'
 import cors from 'cors'
 import {pool} from "./db.mjs"
-import {Server} from 'socket.io'
+// import {Server} from 'socket.io'
 
 const app = express()
 app.use(cors());
 app.use(express.json())
 
-const server = http.createServer(app)
-const io = new Server(server,{
-  cors:{
-    origin:"http://localhost:5173",
-    methods:["GET","POST"]
-  }
-})
+// const server = http.createServer(app)
+// const io = new Server(server,{
+//   cors:{
+//     origin:"http://localhost:5173",
+//     methods:["GET","POST"]
+//   }
+// })
 
 
 
@@ -43,20 +43,20 @@ app.use('/api/lobby', LobbyRoute)
 app.use('/api/messages', messagesRoute)
 app.use('/api/user', userRoute)
 
-io.on('connection', (socket) => {
-  console.log(`user connected: ${socket.id}`);
+// io.on('connection', (socket) => {
+//   console.log(`user connected: ${socket.id}`);
 
 
-socket.on('send_message', (data) => {
-    console.log("message recieved on lobby", data.lobbyId);
-    socket.broadcast.emit("receive_message", data)
+// socket.on('send_message', (data) => {
+//     console.log("message recieved on lobby", data.lobbyId);
+//     socket.broadcast.emit("receive_message", data)
 
-  });
+//   });
 
-  socket.on('create_lobby', (data) => {
-  console.log("lobby created", data.name);
-  socket.broadcast.emit("new_lobby_created", data)
-  });
-});
+//   socket.on('create_lobby', (data) => {
+//   console.log("lobby created", data.name);
+//   socket.broadcast.emit("new_lobby_created", data)
+//   });
+// });
 
-server.listen(process.env.PORT || 5000, () => console.log('ready to serve...'))
+app.listen(process.env.PORT || 5000, () => console.log('ready to serve...'))
