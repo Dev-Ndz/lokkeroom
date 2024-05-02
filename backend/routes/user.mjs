be4c8054b54fc65a1516c8bfce899e815e2f345e
ndz <nicolas.dzierzgowski@gmail.com>
2024-05-02 14:16:27 +0200

    fix

--- backend/routes/user.mjs
+++ backend/routes/user.mjs
@@ -1,11 +1,11 @@
 import express from 'express'
-import {getLobbies, getUserList}from '../controller/userController.mjs'
+import {getLobbies, }from '../controller/userController.mjs'
 
 const router = express.Router();
 
 router.get('/lobbies',getLobbies);
 //router.get('/lobbies',async (req, res) => { res.send("hello from user/lobbies")});
-router.get('/user-list/:lobby_id',getUserList);
+
 
 
 export const userRoute = router;
